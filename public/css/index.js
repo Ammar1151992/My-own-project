18e8cc1819b74580092725ce3ea8382de6f16316
ammar alani <ammaralani@smarts-MacBook-Air.local>
2023-02-05 18:31:24 +0300

    comleted the footer

--- public/css/index.js
+++ public/css/index.js
@@ -1,53 +1,85 @@
-/ This code for the footer
+//This code for the footer
 const template = document.createElement("template");
 
-template.innerHTML = 
-<div class="mt-5 pt-5 pb-5 footer d-flex align-items-center">
-<div class="container">
-  <div class="row main-footer">
-    <div class="col-lg-5 col-xs-12 about-company">
-      <h2>Heading</h2>
-      <p class="pr-5 text-white-50">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
-        ante mollis quam tristique convallis
-      </p>
-      <p>
-        <a href="#"><i class="fa fa-facebook-square mr-1"></i></a
-        ><a href="#"><i class="fa fa-linkedin-square"></i></a>
-      </p>
-    </div>
-    <div class="col-lg-3 col-xs-12 links">
-      <h4 class="mt-lg-0 mt-sm-3">Links</h4>
-      <ul class="m-0 p-0">
-        <li>- <a href="#">Lorem ipsum</a></li>
-        <li>- <a href="#">Nam mauris velit</a></li>
-        <li>- <a href="#">Etiam vitae mauris</a></li>
-        <li>- <a href="#">Fusce scelerisque</a></li>
-        <li>- <a href="#">Sed faucibus</a></li>
-        <li>- <a href="#">Mauris efficitur nulla</a></li>
-      </ul>
-    </div>
-    <div class="col-lg-4 col-xs-12 location">
-      <ul>
-        <li><a href="#">الرئيسية</a></li>
-        <li>| <a href="#">جدول الدروس</a></li>
-        <li>| <a href="#">الدرجات</a></li>
-        <br />
-        <li><a href="#">الغيابات</a></li>
-        <li>| <a href="#">المحاضرات</a></li>
-        <li>| <a href="#">الاعدادات</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="row mt-5">
-    <div class="col copyright">
-      <p class="">
-        <small class="text-white-50">© 2019. All Rights Reserved.</small>
-      </p>
-    </div>
-  </div>
+template.innerHTML = `
+<div>
+
+  <footer class="footer text-center text-lg-start ">
+  <div class="container d-flex justify-content-center py-3">
+  <table>
+    <tr>
+      <td >
+          <ul class="about-footer">
+            <li><a href="#">تواصل معنا</a></li>
+            <li><a href="#">استفسارات</a></li>
+            <li><a href="#">موقع الجامعة</a></li>
+          </ul>
+   
+      </td>
+      <td>
+        <div class="logo-content">
+          <img class="logo" src="../css/image/logo.png" alt="logo">
+        </div>
+      </td>
+      <td>
+          <ul class="footer-list">
+            <li><spsn class="span-footer"> | </spsn> <a href="#"> الرئيسية </a></li>
+            <li><spsn class="span-footer"> | </spsn> <a href="#" >  جدول الدروس </a></li>
+            <li><spsn class="span-footer"> </spsn> <a href="#"> الدرجات</a></li>
+          </ul>
+          <div class="clear"></div>
+          <ul class="footer-list">
+            <li><spsn class="span-footer"> | </spsn> <a href="#"> الغيابات</a></li>
+            <li><spsn class="span-footer"> | </spsn> <a href="#"> المحاضرات</a></li>
+            <li><spsn class="span-footer">  </spsn> <a href="#"> الاعدادات</a></li>
+        </ul>
+        <div class="clear"></div>
+      </td>
+    </tr>
+  </table>
 </div>
+
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3 text-footer" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 All Rights Reserved by Baghdad unversity
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
 </div>
+<!-- End of .container -->
 `;
 
 document.body.appendChild(template.content);
+
+
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
